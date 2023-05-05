1cc8f38e7113fff13a6d2bf3a0d5a369fe8e44f0
JoseR200 <85463913+JoseR200@users.noreply.github.com>
2023-05-04 23:32:23 -0500

    Update ArticleController.js

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -1,3 +1,6 @@
+const validator = require("validator")
+const Article = require("../models/ArticleModel")
+
 const test = (req, res) => {
     return res.status(200).json({
         message: "I'm a test action in the articles controller"
@@ -18,10 +21,35 @@ const course = (req, res) => {
 const save = (req, res) => {
     let params = req.body;
 
-    return res.status(200).json({
-        message: "Save action",
-        params
-    })
+    try{
+        let validate_title = !validator.isEmpty(params.title) && 
+                            validator.isLength(params.title, {min: 5, max: undefined});
+        let validate_content = !validator.isEmpty(params.content);
+
+        if(!validate_title || !validate_content){
+            throw new Error("Couldn't validate the info");
+        }
+    } catch(error){
+        return res.status(400).json({
+            status: "error",
+            message: "Data not valid"
+        })
+    }
+
+    const article = new Article(params);
+
+    article.save().then((articleSaved) => {
+        return res.status(200).json({
+            status: "Success",
+            article: articleSaved,
+            message: "Article was saved successfully!!"
+        })
+    }).catch((error) => {
+        return res.status(400).json({
+            status: "error",
+            message: "Article was not saved..."
+        })
+    });
 }
 
 module.exports = {
